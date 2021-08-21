3d1494d4a29fcb9fd8f0114a3af967182debae1d
Kastango <Metroidling@gmail.com>
2021-08-21 19:34:37 -0300

    fixed login typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,9 @@ export default function Home() {
   return (
     
     <Flex height="100vh"  alignItems="center" justifyContent="center">
-      <Flex direction="column" width="360px" background="gray.100" p={12} rounded={6} >
+      <Flex direction="column" width={["360px", "450px"]} background="gray.100" p={12} rounded={6} >
         <Heading mb={6}>Login</Heading>
-        <Input onChange={event => setSlug(event.target.value)} placeholder="aluno / servidor / visitante" variant="filled" mb={3} type="email" />
+        <Input onChange={event => setSlug(event.target.value)} placeholder="aluno / servidor / visitante / admin" variant="filled" mb={3} type="email" />
         <Input placeholder="***********" variant="filled" mb={6} type="password" />
         <Button onClick={handleClick} colorScheme="teal">Log in</Button>
       </Flex>
