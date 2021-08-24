b54dc62631841edbb3be2d2baf1b70d4bb448623
Kastango <Metroidling@gmail.com>
2021-08-24 09:17:27 -0300

    key added in profile

--- components/profile.tsx
+++ components/profile.tsx
@@ -8,7 +8,7 @@ export default function ProfileComponent(props) {
                 <Stack spacing={4} direction="column" mx={[1, 2, 20]} mt={8} w='full' background="gray.100" p={[6, 12]} rounded={6} >
                     <Heading mb={4}>Seu Perfil</Heading>
                     {props.titles.map(title => (
-                        <Stack spacing={4} direction="row" alignItems="Center">
+                        <Stack key={title} spacing={4} direction="row" alignItems="Center">
                             <Text fontSize="lg" fontWeight="bold">{title.title}</Text>
                             <InputGroup variant="outline" colorScheme="teal">
                                 <Input type="string" placeholder={props.mock[title.title.toLowerCase()]} />
